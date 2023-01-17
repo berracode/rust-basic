729a0f7cde6a5fabf735f574533b9d52846d6b32
QuantumInformatik <juanguillermoalarcon@gmail.com>
2023-01-16 20:47:21 -0500

    Tipos de datos escalares en rust

--- tipos_datos/src/main.rs
+++ tipos_datos/src/main.rs
@@ -1,12 +1,47 @@
 fn main() {
-    // ENTEROS y ENTEROS LITERALES
+    //ENTEROS 
+    let integer: i32 = 20000;
 
-    let age: u8 = 78;
-    println!("el valor de age es: {age}");
+    // 
+    let age: i8 = 12;
+    let age: u8 = 255;
+    //let age: u8 = 256; //error de desbordamiento. 
 
-    let max_value: i32 = 2_147_483_647;
-    println!("max_value {max_value}");
 
-    //esto da error, hemos sobrepasado el rango
-    //let max_value_plus_one: i32 = 2_147_483_648;
+    //ENTEROS LITERALES
+
+    let max_value_i32 = 2147483647;
+    println!("1. max_value entero: {}", max_value_i32);
+    let max_value_i32 = 2_147_483_647;
+    println!("2. max_value entero literal: {}", max_value_i32);
+
+
+    //FLOTANTES (f32 y f64 y ambos son con signo)
+
+    let x = 200000000.0;
+    let y: f32 = 3.0;
+    let z = 2.0;
+
+    //let w: f32 = 3; //error de coincidencia de tipos, no existe el casting automatico. 
+
+    //FLOTANTES CON SEPARADOR
+
+    let float_with_separator = -11_595_384.230_000;
+
+    println!("float_with_separator {}", float_with_separator);
+
+    // BOOLEAN
+
+    let is_fun: bool = true;
+    println!("Is Rust Fun? {is_fun}");
+
+    //CARACTER O CHAR
+
+    let c: char = 'z';
+    let d = 'Æ'; //alt 146
+    let e = '#';
+
+
+
+
 }
