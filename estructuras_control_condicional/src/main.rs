01c1a2d5f71e699db6e3eafb626fc531c45e4f94
QuantumInformatik <juanguillermoalarcon@gmail.com>
2023-01-22 21:33:01 -0500

    control de flujo condicional

---
+++ estructuras_control_condicional/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    // expresiones if
+
+    let is_rust_fun = true;
+
+    if is_rust_fun == true {
+        println!("Rust is fun");
+    }else{
+        println!("Rust is not fun")
+    }
+
+    let number = 101;
+
+    if number != 10 {
+        println!("El numero no es 10");
+    }else {
+        println!("El numero es 10")
+    }
+
+
+    let numero  =  3;
+
+    if (numero % 4) == 0 {
+        println!("el numero es divisible por 4");
+    } else if numero % 3 == 0 {
+        println!("el numero es divisible por 3");
+    } else if numero % 2 == 0{
+        println!("el numero es divisible por 2");
+    } else {
+        println!("el numero no es divisible ni por 4, 3 o 2");
+    }
+
+    
+
+}
