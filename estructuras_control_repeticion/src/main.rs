39a94a1bf1a616f0b157b8b23c1a2f059e3b14fd
QuantumInformatik <juanguillermoalarcon@gmail.com>
2023-01-24 22:20:02 -0500

    estructuras de control de repetición, loop labels, while y for

--- estructuras_control_repeticion/src/main.rs
+++ estructuras_control_repeticion/src/main.rs
@@ -18,7 +18,58 @@ fn main() {
         }
     };
 
+    //loop copn etiquetas o labels
+
+    let mut conteo = 0;
+
+    'contando: loop {
+        println!("Conteo: {conteo}");
+
+        let mut restante = 10;
+
+        loop {
+            println!("Restante: {restante}");
+            if restante == 9 {
+                break;
+            }
+
+            if conteo == 2 {
+                break 'contando;
+            }
+
+            restante-=1;
+        }
+
+        conteo += 1;
+
+    }
+
+    println!("Conteo final: {}", conteo);
+
     println!("El resultado es: {}", resultado);
+
+
+    //bucles con while
+
+    let mut incremental = 0;
+
+    while incremental < 10 {
+
+        println!("incremental {}", incremental);
+
+        incremental += 1;
+        
+    }
+
+
+    // recorriendo array
+
+    let enteros = [3,5,9,90,100];
+
+    for elemento in enteros {
+        println!("elemento {}", elemento);
+        
+    }
 
 
 }
