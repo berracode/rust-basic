abef880e367e248b69cb4564a3dcb45993f8793b
QuantumInformatik <juanguillermoalarcon@gmail.com>
2023-01-26 23:46:45 -0500

    Se agrega código para explicar el stack

---
+++ ownership/main1.c
@@ -0,0 +1,10 @@
+
+void funcion2(){
+    int b = 34;
+    int c = 34;
+}
+
+int main (int argc, char *argv[]){
+    int a = 34;
+    funcion2();
+}
---
+++ ownership/src/main.rs
@@ -0,0 +1,13 @@
+
+
+fn main() {
+    //valores en el stack
+    let a = 42;
+    funcion_2();
+}
+
+fn funcion_2(){
+    let b = 78;
+    let c = 12;
+
+}
